d870a1e01df39e4730df6f51fe0601dd084bb510
AiratTop <airat.halitov@gmail.com>
2026-03-26 21:04:48 +0300

    Format JSON

--- worker.js
+++ worker.js
@@ -76,7 +76,7 @@ function generateUuid(version) {
 }
 
 function jsonResponse(data, status = 200) {
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify(data, null, 2), {
     status,
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
